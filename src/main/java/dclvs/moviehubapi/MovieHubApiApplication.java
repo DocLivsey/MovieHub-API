d9f4e228755d68cbf5e68b0314c51d40a9352a4b
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-16 18:09:30 +0300

    init spring project

---
+++ src/main/java/dclvs/moviehubapi/MovieHubApiApplication.java
@@ -0,0 +1,13 @@
+package dclvs.moviehubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieHubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieHubApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dclvs/moviehubapi/MovieHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dclvs.moviehubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieHubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
