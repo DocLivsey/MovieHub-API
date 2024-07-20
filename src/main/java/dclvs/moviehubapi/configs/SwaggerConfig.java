e9090b514a22749a7ac6c7bc8068777a62102e91
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-20 19:20:18 +0300

    complete custom swagger ui header

--- src/main/java/dclvs/moviehubapi/configs/SwaggerConfig.java
+++ src/main/java/dclvs/moviehubapi/configs/SwaggerConfig.java
@@ -2,19 +2,30 @@ package dclvs.moviehubapi.configs;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import java.io.IOException;
+import java.util.Properties;
 
 @Configuration
+@RequiredArgsConstructor
+@PropertySource("/application.properties")
 public class SwaggerConfig {
 
+    private final Environment environment;
+
     @Bean
-    public OpenAPI swaggerDefinition() {
+    public OpenAPI swaggerDefinition() throws IOException {
         return new OpenAPI()
                 .info(new Info()
                         .title("Movie Hub API")
-                        .description("${spring.application.description}")
-                        .version("${spring.application.version}"));
+                        .description(environment.getProperty("spring.application.description"))
+                        .version(environment.getProperty("spring.application.version")));
     }
 
 }
