886caaac39ced7e37a0bd278959be442e602f235
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-20 19:07:47 +0300

    create custom swagger ui with props from application.properties

---
+++ src/main/java/dclvs/moviehubapi/configs/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package dclvs.moviehubapi.configs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI swaggerDefinition() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Movie Hub API")
+                        .description("${spring.application.description}")
+                        .version("${spring.application.version}"));
+    }
+
+}
