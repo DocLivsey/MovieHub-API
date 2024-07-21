f985247953a098741a5097252a8b72de3e8b19b7
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-21 03:28:43 +0300

    add method for handle GET movie by id endpoint

--- src/main/java/dclvs/moviehubapi/clients/KinopoiskDevAPIClient.java
+++ src/main/java/dclvs/moviehubapi/clients/KinopoiskDevAPIClient.java
@@ -1,7 +1,15 @@
 package dclvs.moviehubapi.clients;
 
+import dclvs.moviehubapi.dto.MovieResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "${kinopoisk.dev.api.client.name}", url = "${kinopoisk.dev.api.url}")
 public interface KinopoiskDevAPIClient {
+
+    @GetMapping("${kinopoisk.dev.api.path}/{id}")
+    MovieResponse getMovie(@PathVariable Long id);
+
 }
