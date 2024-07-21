f32d7b215bd7c34297da2b24cd97c030d7fb9aec
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-07-21 03:29:14 +0300

    finished method for getting movie by id

--- src/main/java/dclvs/moviehubapi/controllers/MovieController.java
+++ src/main/java/dclvs/moviehubapi/controllers/MovieController.java
@@ -1,9 +1,11 @@
 package dclvs.moviehubapi.controllers;
 
+import dclvs.moviehubapi.clients.KinopoiskDevAPIClient;
 import dclvs.moviehubapi.dto.MovieResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Controller for work with movies", description = "API для обращения к KinopoiskAPI")
 public class MovieController {
 
+    private final KinopoiskDevAPIClient kinopoiskDevAPIClientImpl;
+
     @GetMapping("/movies/{id}")
-    public ResponseEntity<MovieResponse> getMovieById(@PathVariable int id) {
-        return null;
+    public ResponseEntity<MovieResponse> getMovieById(@PathVariable Long id) {
+        MovieResponse movieResponse = kinopoiskDevAPIClientImpl.getMovie(id);
+        log.info("Movie response: {}", movieResponse);
+        return ResponseEntity.ok(movieResponse);
     }
 
 }
