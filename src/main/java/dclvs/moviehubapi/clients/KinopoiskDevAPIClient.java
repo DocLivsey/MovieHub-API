331f6b5a1a3ff92b118b9b4e601a56d925ae7dac
DocLivsey <doclivsey@users.noreply.github.com>
2024-12-26 18:55:27 +0300

    feat: improve uri props refs

--- src/main/java/dclvs/moviehubapi/clients/KinopoiskDevAPIClient.java
+++ src/main/java/dclvs/moviehubapi/clients/KinopoiskDevAPIClient.java
@@ -1,7 +1,6 @@
 package dclvs.moviehubapi.clients;
 
 import dclvs.moviehubapi.dto.dev.MovieResponse;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 @FeignClient(name = "${kinopoisk.dev.api.client.name}", url = "${kinopoisk.dev.api.url}")
 public interface KinopoiskDevAPIClient {
 
-    @GetMapping(value = "${kinopoisk.dev.api.path}/{id}")
+    @GetMapping(value = "${kinopoisk.dev.api.version}${kinopoisk.dev.api.path.movie}/{id}")
     MovieResponse getMovieById(@PathVariable("id") Long id,
                                @RequestHeader("X-API-KEY") String token);
 
